1208880a60c03e1082111c023e799ac1add9ecd7
jamgreen <jamaal.green@gmail.com>
2017-11-06 06:44:10 -0800

    created new nad83 sub-folder under spatial data

--- processing_scripts/join_lehd_to_cities.R
+++ processing_scripts/join_lehd_to_cities.R
@@ -7,7 +7,7 @@ options(tigris_class = "sf", tigris_use_cache = TRUE)
 
 
 
-cities_50 <- st_read("./data/spatial/places_nad83.shp")
+cities_50 <- st_read("./data/spatial/NAD83/places_nad83.shp")
 
 cities_50 <- cities_50 %>% select(3:5)
 
@@ -17,8 +17,8 @@ lehd_sf <- readRDS("./data/spatial/lehd_cbsa_sf.RDS")
 lehd_cities <- st_join(lehd_sf, cities_50, join = st_intersects, left = FALSE)
 lehd_cities <- lehd_cities %>% select(-c(25:33))
 
-saveRDS(lehd_cities, "./data/spatial/lehd_citiesNAD83.rds")
-st_write(lehd_cities, "./data/spatial/lehd_citiesNAD83.geojson")
+saveRDS(lehd_cities, "./data/spatial/NAD83/lehd_citiesNAD83.rds")
+st_write(lehd_cities, "./data/spatial/NAD83/lehd_citiesNAD83.geojson")
 
 rm(list = ls())
 
