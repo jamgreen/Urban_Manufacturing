c0c24fe886caf7db413cda017ef059a3612f0e62
jamgreen <jamaal.green@gmail.com>
2017-11-18 15:24:40 -0800

    add 2002 lehd data tables

--- processing_scripts/get_lehd.R
+++ processing_scripts/get_lehd.R
@@ -18,7 +18,7 @@ states_distinct <- study_states %>%  distinct(State)
 
 #download 2004 and 2015 to the data/lehd folder
 
-years <- 2004:2015
+years <- 2002:2015
 
 big_lehd <- grab_lodes(state = states_distinct$State, year = years, 
                        lodes_type = "wac", job_type = "JT01", segment = "S000",
