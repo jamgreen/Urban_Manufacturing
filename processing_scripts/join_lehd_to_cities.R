fa5b69844d2bb605e6a6ac3102128fa43cb7174f
jamgreen <jamaal.green@gmail.com>
2017-11-22 10:21:20 -0800

    getting everything set for upload into postgis

--- processing_scripts/join_lehd_to_cities.R
+++ processing_scripts/join_lehd_to_cities.R
@@ -17,8 +17,10 @@ lehd_sf <- readRDS("./data/spatial/lehd_cbsa_sf.RDS")
 lehd_cities <- st_join(lehd_sf, cities_50, join = st_intersects, left = FALSE)
 lehd_cities <- lehd_cities %>% select(-c(25:33))
 
+lehd_cities_albers <- st_transform(lehd_cities, 5070)
+
 saveRDS(lehd_cities, "./data/spatial/NAD83/lehd_citiesNAD83.rds")
-st_write(lehd_cities, "./data/spatial/NAD83/lehd_citiesNAD83.geojson")
+st_write(lehd_cities_albers, "./data/spatial/NAD83/lehd_cities_NAD83Albers.shp", delete_dsn = TRUE)
 
 rm(list = ls())
 
