466f91c051fb69083fe8694c16838647966b685a
jamgreen <jamaal.green@gmail.com>
2018-01-11 13:42:23 -0800

    made final propensity score table with stabilized and unstabilized weights and exported that table into the model folder in the data director

--- PropensityScore_approach/propensity_weight_calcs.R
+++ PropensityScore_approach/propensity_weight_calcs.R
@@ -1,6 +1,6 @@
 
 if(!require(pacman)){install.packages("pacman"); library(pacman)}
-p_load(RPostgreSQL, car, brglm, lmtest, sandwich, tableone, survey, dplyr, dbplyr)
+p_load(RPostgreSQL, car, brglm, lmtest, sandwich, tableone, ipw, survey, dplyr, dbplyr)
 
 
 #logistic table processing from industrial_land db for MANUFACTURING JOBS ONLY------
@@ -57,19 +57,15 @@ city_lehd_mfg$RenterPer[is.nan(city_lehd_mfg$RenterPer)] <- 0
 city_dist <- tbl(con, "city_dist_km")
 city_dist <- collect(city_dist)
 
-city_lehd_mfg <- city_lehd_mfg %>% left_join(city_dist, by = c("blk_grp_id" = "blk_grp_id")) 
+city_lehd_mfg <- city_lehd_mfg %>% left_join(city_dist, by = c("blk_grp_id" = "blk_grp_id")) %>% 
+  distinct(blk_grp_id, .keep_all = TRUE)
+
+city_lehd_mfg$pmd <- ifelse(city_lehd_mfg$pmd_dummy == TRUE, 1, 0)
 
 #city_lehd_mfg <- city_lehd_mfg[!duplicated(city_lehd_mfg$blk_grp_id), ] 
 
 
 #logistic regression for calculating propensity weights----
-
-
-# log_m1_brglm <- brglm(pmd_dummy ~ MfgShare + BlackPer + HispPer + RenterPer + dist_km + pop_density, 
-# data = city_lehd_mfg, family = binomial(link = "logit"), method = "brglm.fit")
-
-#log_mfg_brglm <- brglm(pmd_dummy ~ MfgShare + BlackPer + HispPer + RenterPer + dist_km + pop_density + 
-#                         mfg_emp, data = city_lehd_mfg, family = binomial(link = "logit"))
 
 log_mfg_glm <- glm(pmd_dummy ~ MfgShare + BlackPer + HispPer + RenterPer + dist_km + network_density, 
                    data = city_lehd_mfg, family = binomial(link = "logit"))
@@ -87,15 +83,50 @@ city_lehd_mfg$mfg_ps <- ifelse(city_lehd_mfg$pmd_dummy == TRUE, 1/city_lehd_mfg$
 city_lehd_mfg$ind_ps <- ifelse(city_lehd_mfg$pmd_dummy == TRUE, 1/city_lehd_mfg$ind_log_val,
                                     1/(1 - city_lehd_mfg$ind_log_val))
 
-#checking to see if re-balances
-prop_mfg_svy <- svydesign(ids = ~ 1, data = city_lehd_mfg, weights = ~ city_lehd_mfg$mfg_ps)
-vars <- c("tot_emp", "mfg_emp", "ind_emp", "MfgShare", "IndShare")
-prof_mfg_table <- svyCreateTableOne(vars = vars, strata = "pmd_dummy", data = prop_mfg_svy, test = FALSE)
+#using ipw to compare the weights calculation-----
+temp_mfg_stabilized <- ipwpoint(exposure = pmd_dummy, family = "binomial", link = "logit",
+                                numerator = ~ 1, 
+                                denominator = ~ MfgShare + BlackPer + HispPer + RenterPer + dist_km + network_density,
+                                data = city_lehd_mfg)
+
+temp_mfg_unstabilized <- ipwpoint(exposure = pmd_dummy, family = "binomial", link = "logit",
+                                  denominator = ~ MfgShare + BlackPer + HispPer + RenterPer + dist_km + network_density,
+                                  data = city_lehd_mfg)
+
+
+temp_ind_stable <- ipwpoint(exposure = pmd_dummy, family = "binomial", link = "logit",
+                            numerator = ~ 1, 
+                            denominator = ~ IndShare + BlackPer + HispPer + RenterPer + dist_km + 
+                              ind_emp,
+                            data = city_lehd_mfg)
+
+temp_ind_unstable <- ipwpoint(exposure = pmd_dummy, family = "binomial", link = "logit",
+                              denominator = ~ IndShare + BlackPer + HispPer + RenterPer + dist_km + network_density,
+                              data = city_lehd_mfg)
+
+city_lehd_mfg$ipw_ind_stable <- temp_ind_stable$ipw.weights
+city_lehd_mfg$ipw_ind_unstable <- temp_ind_unstable$ipw.weights
+
+city_lehd_mfg$ipw_mfg_stable <- temp_mfg_stabilized$ipw.weights
+city_lehd_mfg$ipw_mfg_unstable <- temp_mfg_unstabilized$ipw.weights
+
+#testing balance with cobalt package
+covs1 <- city_lehd_mfg %>% select(MfgShare, BlackPer, HispPer, RenterPer, dist_km, network_density)
+
+bal.tab(covs1, treat = city_lehd_mfg$pmd, weights = city_lehd_mfg$ipw_mfg_stable,
+        method = "weighting")
+
+covs2 <- city_lehd_mfg %>% select(IndShare, BlackPer, HispPer, RenterPer, dist_km, network_density)
+
+bal.tab(covs2, treat = city_lehd_mfg$pmd, weights = city_lehd_mfg$ipw_ind_stable,
+        method = "weighting")
+
 
 #Create new propensity score table for manufacturing and industrial employment
 
 prop_gt_table <- city_lehd_mfg %>% 
-  select(geoid10 = blk_grp_id, pmd_dummy, mfg_ps, ind_ps, city_name)  
+  select(geoid10 = blk_grp_id, pmd_dummy, mfg_ps, ipw_mfg_stable, ipw_mfg_unstable ,
+         ind_ps, ipw_ind_stable, ipw_ind_unstable , city_name)  
   
 
 copy_to(con, prop_gt_table ,"prop_score_mfg_ind", temporary = FALSE, 
@@ -109,29 +140,22 @@ prop_2009 <- collect(prop_2009)
 prop_2015 <- tbl(con, "lehd_model_2015_emp")
 prop_2015 <- collect(prop_2015)
 
-prop_final <- prop_2015 %>% select(1, 5:12) %>% inner_join(prop_2009, by = "geoid10")
+prop_final <- prop_2015 %>% select(1, 12:17) %>% inner_join(prop_2009, by = "geoid10")
+
 prop_final <- prop_final %>% mutate(mfg_change = mfg2015 - mfg2009,
             ind_2009 = ag2009 + mining2009 + util2009 + mfg2009 + wholesale2009 + transpo2009,
             ind_2015 = ag2015 + mining2015 + util2015 + mfg2015 + wholesale2015 + transpo2015,
             ind_change = ind_2015 - ind_2009)
 
-prop_final <- prop_final %>% select(geoid10, city = city_name.x, bg_fips = bg_fips.x,
-                                    pmd_dummy, prop_score_mfg, prop_score_ind,
+prop_final <- prop_final %>% select(geoid10, city = city_name, bg_fips,
+                                    pmd_dummy, mfg_ps, ipw_mfg_stable, ipw_mfg_unstable,
+                                    ind_ps, ipw_ind_stable, ipw_ind_unstable,
                                     mfg_change, ind_change)
 
+readr::write_csv(prop_final, "data/model/propensity_score_mfg_ind_table.csv")
 
-prop_final_mfg <- svydesign(ids = ~ 1, weights = ~ prop_score_mfg, data = prop_final)
 #dbClearResult(con)
 dbDisconnect(con)
 
-
-# running the models  city clustered SE
-
-prop_mfg1 <- lm(mfg_change ~ pmd_dummy, weights = prop_score_mfg,
-                data = prop_final)
-
-prop_mfg1_svy <- svyglm(mfg_change ~ pmd_dummy, design = prop_final_mfg)
-
-prop_mfg2 <- lm(mfg_change ~ pmd_dummy + factor(geoid10), weights = prop_score_mfg,
-                data = prop_final)
+rm(list = ls())
 
