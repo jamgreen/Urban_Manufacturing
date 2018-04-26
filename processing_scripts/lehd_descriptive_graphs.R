d70539c40456ee3de27d5cc5b6eda54ee4def8fd
jamgreen <jamaal.green@gmail.com>
2018-04-26 11:44:27 -0700

    safe commit

--- processing_scripts/lehd_descriptive_graphs.R
+++ processing_scripts/lehd_descriptive_graphs.R
@@ -58,16 +58,22 @@ city_lehd_mfg$Outlier <- as.factor(city_lehd_mfg$Outlier)
 levels(city_lehd_mfg$Outlier) <- c("Low", "Average", "High")
 
 city_lehd_mfg <- city_lehd_mfg %>% 
-  mutate(Policy = ifelse(namelsad %in% c("Los Angeles city", "Chicago city", "San Diego city", "Jacksonville city", "San Francisco city", "Seattle city", "Baltimore city", "Portland city", "San Jose city", "Minneapolis city", "New York city"), 1, 0))
+  mutate(Policy = ifelse(namelsad %in% c("Los Angeles city", "Chicago city", 
+                                         "San Diego city", "Jacksonville city", 
+                                         "San Francisco city", "Seattle city",
+                                         "Baltimore city", "Portland city", 
+                                         "San Jose city", "Minneapolis city", 
+                                         "New York city"), 1, 0))
 
 city_lehd_mfg <- city_lehd_mfg %>% 
   mutate(city_name = gsub("city", "", city_lehd_mfg$namelsad))
 
 city_lehd_mfg <- city_lehd_mfg %>% 
-  mutate(city_name_short = case_when(city_name == "Indianapolis  (balance)" ~ "Indianapolis",
-                                     city_name == "Louisville/Jefferson County metro government (balance)" ~ "Louisville",
-                                     city_name == "Nashville-Davidson metropolitan government (balance)" ~ "Nashville",
-                                     TRUE ~ city_name))
+  mutate(city_name_short = 
+case_when(city_name == "Indianapolis  (balance)" ~ "Indianapolis",
+city_name == "Louisville/Jefferson County metro government (balance)" ~ "Louisville",
+city_name == "Nashville-Davidson metropolitan government (balance)" ~ "Nashville",
+TRUE ~ city_name))
 
 city_lehd_mfg <- city_lehd_mfg %>% 
   mutate(mfg_emp_thousands = cns05/1000)
@@ -98,7 +104,8 @@ mfg_index_plot <- ggplot(city_lehd_mfg, aes(year, mfg_2004_idx, group = city_nam
   theme_minimal() +
   labs(title = "Change in Mfg Employment for 48 Large Cities, 2004-2015",
        subtitle = "Mfg. Employment Indexed to 2004 measures",
-       x = "Year", y = "Mfg. Employment Index", caption = "Source: LEHD WAC Files 2004-2015") +
+       x = "Year", y = "Mfg. Employment Index", 
+       caption = "Source: LEHD WAC Files 2004-2015") +
   theme(legend.position = "none", panel.grid = element_blank())
 
 
@@ -107,7 +114,8 @@ mfg_index_facet <- ggplot(city_lehd_mfg, aes(year, mfg_2004_idx, group = city_na
   geom_line(color = "blue3") + theme_minimal() +
   labs(title = "Change in Mfg Employment for 48 Large Cities, 2004-2015",
        subtitle = "Mfg. Employment Indexed to 2004 measures",
-       x = "Year", y = "Mfg. Employment Index", caption = "Source: LEHD WAC Files 2004-2015",
+       x = "Year", y = "Mfg. Employment Index", 
+       caption = "Source: LEHD WAC Files 2004-2015",
        axis.text.y = 4) +
   theme(legend.position = "none", panel.grid = element_blank()) +
   geom_hline(yintercept = 100, color = "red3", linetype = "dotted") +
@@ -226,6 +234,7 @@ city_lehd_ind <- city_lehd_ind %>%
   mutate(emp_trend = case_when(ind_emp_change < 0 ~ "Decreasing Trend",
                                ind_emp_change >= 0 ~ "Increasing Trend"))
 
+#industriel total employment line---------------
 
 ind_tot_emp_line <- ggplot(city_lehd_ind, 
                            aes(year, ind_emp_thousands, 
@@ -271,11 +280,122 @@ ind_share_emp_lolli <- ggplot(ind_2015,
   theme(panel.grid.minor = element_blank()) +
   coord_flip()
 
+rm(city_lehd_ind)
+#pmd_differences-----------
 
-#pmd_differences
-
-city_pmd_emp <- city_lehd %>% select(1:8, cns05, cns06, cns08, placefp, namelsad, pmd_policy) %>% 
+city_pmd_emp <- city_lehd %>% 
+  select(1:8, cns05, cns06, cns08, placefp, namelsad, pmd_policy) %>% 
   group_by(namelsad, pmd_policy, year) %>% summarise_if(is.numeric, sum)
+
+
+city_pmd_emp <- city_pmd_emp %>% 
+  mutate(ind_emp = cns01 + cns02 + cns03 + cns05 + cns06 + cns08)
+
+city_pmd04 <- city_pmd_emp %>% filter(year == 2004) %>% 
+  select(namelsad, pmd_policy, mfg_2004 = cns05, ind_2004 = ind_emp)
+
+city_pmd_emp <- city_pmd_emp %>% 
+  left_join(city_pmd04, 
+            by = c("namelsad" = "namelsad", "pmd_policy" = "pmd_policy"))
+
+city_pmd_emp <- city_pmd_emp %>% 
+  mutate(mfg_idx = (cns05/mfg_2004)*100, ind_idx = (ind_emp/ind_2004)*100)
+
+pmd_cities <- city_pmd_emp %>%  
+  filter(namelsad %in% c("Baltimore city", "Chicago city", "San Diego city", 
+                         "New York city",
+                         "Jacksonville city", "Seattle city", "San Jose city",
+                         "Minneapolis city",
+                         "San Francisco city", "Portland city"))
+
+pmd_cities <- pmd_cities %>% 
+  mutate(city_name = stringr::str_replace(namelsad, "city", ""))
+
+
+pmd_lehd <- city_lehd %>% 
+  filter(namelsad%in% c("Baltimore city", "Chicago city", "San Diego city", 
+                        "New York city",
+                        "Jacksonville city", "Seattle city", "San Jose city",
+                        "Minneapolis city","San Francisco city", 
+                        "Portland city")) %>% 
+  mutate(city_name = stringr::str_replace(namelsad, "city", ""))
+
+#mean/median mfg and industrial employment by pmd-----
+
+pmd_emp <- city_lehd %>% 
+  select(1:8, cns05, cns06, cns08, placefp, namelsad, pmd_policy) 
+
+pmd_emp <- pmd_emp %>% 
+  mutate(ind_emp = cns01 + cns02 + cns03 + cns05 + cns06 + cns08) %>% 
+  mutate(city_name = stringr::str_replace(namelsad, "city", ""))
+
+
+
+#pmd vs non-pmd 
+
+pmd_med_emp <- pmd_emp %>% filter(ind_emp > 0) %>% 
+  group_by(pmd_policy, year) %>% 
+  mutate(mfg_per = cns05/c000, ind_per = ind_emp/c000) %>% 
+  summarise(avg_mfg_emp = mean(cns05, na.rm = TRUE),
+            med_mfg_emp = median(cns05, na.rm = TRUE),
+            avg_ind_emp = mean(ind_emp, na.rm = TRUE),
+            med_ind_emp = median(ind_emp, na.rm = TRUE),
+            avg_mfg_per = mean(mfg_per, na.rm = TRUE), 
+            avg_ind_per = mean(ind_per, na.rm = TRUE))
+
+
+  
+  
+#pmd faceted line plot and 2015 boxplot-----
+
+pmd_mfg_idx_facet <- ggplot(pmd_cities, aes(x = year, y = mfg_idx, 
+                            group = pmd_policy,
+                            fill = pmd_policy, color = pmd_policy)) 
+
+pmd_mfg_idx_facet <- pmd_mfg_idx_facet + geom_line() + theme_minimal() + 
+  theme(legend.position = "bottom", 
+        panel.grid = element_blank(),
+        axis.text.x = element_text(size = 5),
+        axis.text.y = element_text(size = 6),
+        legend.title = element_blank()) +
+  labs(title = "Change in Mfg. Employment in Cities with PMDs, 2004-2015",
+       x = "Year", y = "Manufacturing Employment index (2004)", 
+       caption = "Source: LEHD WAC Files 2004-2015",
+       fill = "PMD Policy") +
+  facet_wrap(~ city_name, ncol = 2, scales = "free") +
+  scale_x_discrete(limits = c("2004", "2007", "2010", "2015")) +
+  scale_y_continuous(labels = scales::comma)
+
+
+
+pmd_mfg_emp_share <- ggplot(pmd_med_emp, aes(x = year, y = avg_mfg_per,
+                        group = pmd_policy, fill = pmd_policy, 
+                        color = pmd_policy)) +
+  geom_line() + theme_minimal() +
+  theme(legend.position = "bottom",
+        panel.grid = element_blank(),
+        axis.text.x = element_text(size = 5),
+        axis.text.y = element_text(size = 6)) +
+  labs(title = "PMD Average Mfg. Employment Share is Much Higher than Non-PMDs",
+       x= "Year", y = "Average Employment Share",
+       caption = "Source: LEHD WAC Files 2015") +
+  scale_y_continuous(labels = scales::percent) +
+  scale_shape_manual(guide_legend(title = "PMD Policy"))
+
+pmd_ind_emp_share <- ggplot(pmd_med_emp, 
+                      aes(x = year, y = avg_ind_per,group = pmd_policy, 
+                      fill = pmd_policy, color = pmd_policy)) +
+  geom_line() + theme_minimal() +
+  theme(legend.position = "bottom",
+        panel.grid = element_blank(),
+        axis.text.x = element_text(size = 5),
+        axis.text.y = element_text(size = 6),
+        legend.title = "PMD Policy") +
+  labs(title = "PMD Average Employment Share is Much Higher than Non-PMDs",
+       x= "Year", y = "Average Employment Share",
+       caption = "Source: LEHD WAC Files 2015") +
+  scale_y_continuous(labels = scales::percent)
+
 
 
 dbDisconnect(con)
