fff584b4b9c91f5c4db1c9cc654ba5c0bc535fcf
jamgreen <jamaal.green@gmail.com>
2018-10-19 14:34:11 -0700

    ideally fixed modeling tables

--- PropensityScore_approach/propensity_weight_calcs.R
+++ PropensityScore_approach/propensity_weight_calcs.R
@@ -42,7 +42,8 @@ acs <- acs %>%
 #join acs to lehd 
 
 city_lehd_acs <- city_lehd %>% 
-  inner_join(acs, by = c("bg_fips" = "geoid10"))
+  inner_join(acs, by = c("bg_fips" = "geoid10")) %>%
+  data.frame() %>% select(-geom)
 
 #fix pmd_policy vars
 city_lehd_acs$pmd_policy <- if_else(is.na(city_lehd_acs$pmd_policy), "Non-PMD", city_lehd_acs$pmd_policy)
@@ -73,8 +74,6 @@ city_lehd_acs15$owner_per[is.na(city_lehd_acs15$owner_per)] <- 0
 
 #calculate difference between 2009-2015 ind and mfg
 
-city_lehd_acs09 <- city_lehd_acs09 %>% select(-geom) %>% data.frame()
-city_lehd_acs15 <- city_lehd_acs15 %>% select(-geom) %>% data.frame()
 
 city_lehd_acs09 <- city_lehd_acs09 %>% 
   select(bg_fips, cns05, ind_emp)
@@ -101,102 +100,113 @@ city_lehd_acs04 <- city_lehd_acs04 %>%
   inner_join(city_lehd_combine_full, by = "bg_fips")
 
 
-rm(list = c("city_lehd_acs09", "city_lehd_acs15", "city_lehd_combine_full"))
+rm(list = c("city_lehd_acs09", "city_lehd_acs15", "city_lehd_combine_full", "city_lehd"))
 #create a restricted data set limited to block groups with at least 4 industrial jobs
+
 city_lehd_acs04_restricted <- city_lehd_acs04 %>% 
   filter(ind_emp >= median(ind_emp))
 
 #calculate weights, both predicted logit coefficients and inverse probability------
 
-#predicted weights restricted and non-restricted sample
+#predicted weights restricted and non-restricted sample logit-----------
 
-ind_full_logit <- glm(pmd_dummy ~ network_density + dist_km + indshare + owner_per,
-                      data = city_lehd_acs04, family = "binomial")
-
-city_lehd_acs04$logit_wgt_full <- predict(ind_full_logit, type = "response")
-city_lehd_acs04$logit_ipw_full <- if_else(city_lehd_acs04$pmd_dummy == 1,
-                                          1/city_lehd_acs04$logit_wgt_full, 
-                                          1/(1 - city_lehd_acs04$logit_wgt_full))
-
-ind_restricted_logit <-  glm(pmd_dummy ~ network_density + dist_km + indshare + owner_per,
-                             data = city_lehd_acs04_restricted, family = "binomial")
-
-city_lehd_acs04_restricted$logit_wgt_restricted <- predict(ind_restricted_logit, type = "response")
-city_lehd_acs04_restricted$logit_ipw_restricted <- if_else(city_lehd_acs04_restricted$pmd_dummy == 1,
-                                                      1/city_lehd_acs04_restricted$logit_wgt_restricted, 
-                                                      1/(1 - city_lehd_acs04_restricted$logit_wgt_restricted))
+# ind_full_logit <- glm(pmd_dummy ~ network_density + dist_km + indshare + owner_per,
+#                       data = city_lehd_acs04, family = "binomial")
+# 
+# city_lehd_acs04$logit_wgt_full <- predict(ind_full_logit, type = "response")
+# city_lehd_acs04$logit_ipw_full <- if_else(city_lehd_acs04$pmd_dummy == 1,
+#                                           1/city_lehd_acs04$logit_wgt_full, 
+#                                           1/(1 - city_lehd_acs04$logit_wgt_full))
+# 
+# ind_restricted_logit <-  glm(pmd_dummy ~ network_density + dist_km + indshare + owner_per,
+#                              data = city_lehd_acs04_restricted, family = "binomial")
+# 
+# city_lehd_acs04_restricted$logit_wgt_restricted <- predict(ind_restricted_logit, type = "response")
+# city_lehd_acs04_restricted$logit_ipw_restricted <- if_else(city_lehd_acs04_restricted$pmd_dummy == 1,
+#                                                       1/city_lehd_acs04_restricted$logit_wgt_restricted, 
+#                                                       1/(1 - city_lehd_acs04_restricted$logit_wgt_restricted))
 
 
 #until i can fix the ipw issue i'll export the model to csv
 
-readr::write_csv(city_lehd_acs04, "data/model/city_lehd_2004_full_propensity.csv")
-readr::write_csv(city_lehd_acs04_restricted, "data/model/city_lehd_2004_restricted_propensity.csv")
+#readr::write_csv(city_lehd_acs04, "data/model/city_lehd_2004_full_propensity.csv")
+#readr::write_csv(city_lehd_acs04_restricted, "data/model/city_lehd_2004_restricted_propensity.csv")
 
-saveRDS(city_lehd_acs04, "data/model/city_lehd_2004_full_propensity.RDS")
-saveRDS(city_lehd_acs04_restricted, "data/model/city_lehd_2004_restricted_propensity.RDS")
+#saveRDS(city_lehd_acs04, "data/model/city_lehd_2004_full_propensity.RDS")
+#saveRDS(city_lehd_acs04_restricted, "data/model/city_lehd_2004_restricted_propensity.RDS")
 
 #test balance with cobalt
 
 
 
+#using ipw to compare the weights calculation-----
 
-# #using ipw to compare the weights calculation-----
-# mfg_stabilized_full <- ipwpoint(exposure = pmd_dummy, family = "binomial", link = "logit",
-#                                 numerator = ~1,
-#                                 denominator = ~ mfgshare + owner_per + dist_km + 
-#                                   network_density,
-#                                 data = city_lehd_acs04)
-# 
-# 
-# 
-# mfg_stabilized_restricted <- ipwpoint(exposure = pmd_dummy, family = "binomial", 
-#                                       link = "logit",
-#                                       numerator = ~1,
-#                                       denominator = ~ mfgshare +  renter_per + dist_km + 
-#                                         network_density,
-#                                       data = city_lehd_acs04_restricted)
-# 
-# 
-# ind_stable_full <- ipwpoint(exposure = pmd_dummy, family = "binomial", link = "logit",
-#                             numerator = ~ 1, 
-#                             denominator = ~ indshare + owner_per  +
-#                               dist_km + network_density,
-#                             data = city_lehd_acs04_restricted)
-# 
-# ind_stable_restricted <- ipwpoint(exposure = pmd_dummy, family = "binomial", link = "logit",
-#                                   numerator = ~ 1, 
-#                                   denominator = ~ mfgshare + black_per + hispanic_per + 
-#                                   renter_per + dist_km + network_density,
-#                                   data = city_lehd_acs04)
-# 
-# city_lehd_acs04$ipw_ind<- ind_stable_full$ipw.weights
-# city_lehd_acs04$ipw_ind_restrict <- ind_stable_restricted$ipw.weights
-# 
-# city_lehd_acs04$ipw_mfg <- mfg_stabilized_full$ipw.weights
-# city_lehd_acs04$ipw_mfg_restrict <- mfg_stabilized_restricted$ipw.weights
-# 
-# #testing balance with cobalt package-------------
-# covs1 <- city_lehd_acs04 %>% select(MfgShare, BlackPer, HispPer, RenterPer, dist_km, network_density)
-# 
-# bal.tab(covs1, treat = city_lehd_acs04$pmd, weights = city_lehd_acs04$ipw_mfg_stable,
-#         method = "weighting")
-# 
-# covs2 <- city_lehd_acs04 %>% select(IndShare, BlackPer, HispPer, RenterPer, dist_km, network_density)
-# 
-# bal.tab(covs2, treat = city_lehd_acs04$pmd, weights = city_lehd_acs04$ipw_ind_stable,
-#         method = "weighting")
-# 
-# 
-# #Create new propensity score table for manufacturing and industrial employment
-# 
-# prop_gt_table <- city_lehd_acs04 %>% 
-#   select(geoid10 = blk_grp_id, pmd_policy, mfg_ps, ipw_mfg_stable, ipw_mfg_unstable ,
-#          ind_ps, ipw_ind_stable, ipw_ind_unstable , city_name)  
-#   
-# 
-# copy_to(con, prop_gt_table ,"prop_score_mfg_ind", temporary = FALSE, 
-#         indexes = list("geoid10"), overwrite = TRUE )
-# 
+#stabilized weights
+
+mfg_stabilized_full <- ipwpoint(exposure = pmd_dummy, family = "binomial", link = "logit",
+                                numerator = ~1,
+                                denominator = ~ mfgshare + network_density + dist_km + owner_per,
+                                data = city_lehd_acs04)
+
+ 
+ 
+ mfg_stabilized_restricted <- ipwpoint(exposure = pmd_dummy, family = "binomial", 
+                                       link = "logit",
+                                       numerator = ~1,
+                                       denominator = ~ mfgshare +  renter_per + dist_km + 
+                                         network_density,
+                                       data = city_lehd_acs04_restricted)
+ 
+ 
+ ind_stable_full <- ipwpoint(exposure = pmd_dummy, family = "binomial", link = "logit",
+                             numerator = ~ 1, 
+                             denominator = ~ indshare + owner_per  +
+                               dist_km + network_density,
+                             data = city_lehd_acs04)
+ 
+ ind_stable_restricted <- ipwpoint(exposure = pmd_dummy, family = "binomial", link = "logit",
+                                   numerator = ~ 1, 
+                                   denominator = ~ mfgshare + black_per + hispanic_per + 
+                                   renter_per + dist_km + network_density,
+                                   data = city_lehd_acs04_restricted)
+ 
+ city_lehd_acs04$ipw_ind<- ind_stable_full$ipw.weights
+ city_lehd_acs04$ipw_mfg <- mfg_stabilized_full$ipw.weights
+ 
+ 
+ city_lehd_acs04_restricted$ipw_ind_restrict <- ind_stable_restricted$ipw.weights
+ city_lehd_acs04_restricted$ipw_mfg_restrict <- mfg_stabilized_restricted$ipw.weights
+ 
+ 
+ 
+ 
+ #testing balance with cobalt package-------------
+ covs1 <- city_lehd_acs04 %>% select(mfgshare, black_per, hispanic_per, renter_per, dist_km, network_density)
+ 
+ bal.tab(covs1, treat = city_lehd_acs04$pmd_dummy, weights = city_lehd_acs04$ipw_mfg,
+         method = "weighting")
+ 
+ covs2 <- city_lehd_acs04 %>% select(indshare, black_per, hispanic_per, renter_per, dist_km, network_density)
+ 
+ bal.tab(covs2, treat = city_lehd_acs04$pmd_dummy, weights = city_lehd_acs04$ipw_ind,
+         method = "weighting")
+ 
+
+#Create new propensity score table for manufacturing and industrial employment
+
+prop_gt_table <- city_lehd_acs04 %>%
+  select(geoid10 = bg_fips, pmd_policy, 62:72, namelsad)
+
+prop_gt_restricted <- city_lehd_acs04_restricted %>% 
+  select(geoid10 = bg_fips, pmd_policy, 62:72, namelsad)
+
+
+copy_to(con, prop_gt_table ,"prop_score_mfg_ind", temporary = FALSE,
+        indexes = list("geoid10"), overwrite = TRUE )
+
+copy_to(con, prop_gt_restricted,"prop_score_mfg_ind_restricted", temporary = FALSE,
+        indexes = list("geoid10"), overwrite = TRUE )
+
 # #weighted regression models, have to query the db to get 2009 and 2015 emp numbers------
 # #**first steps are to create the final model table----
 # 
